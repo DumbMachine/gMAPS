bf5885b9618b2b420878da506424909c529807b5
DumbMachine <ratin.kumar.2k@gmail.com>
2019-04-12 16:50:39 +0530

    .

--- src/components/map.js
+++ src/components/map.js
@@ -1,15 +1,87 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
 import {geolocated} from 'react-geolocated';
-
+import * as helpers from "@turf/helpers";
 
 class MAP extends Component { 
     state = {
-        lat:0,
+        lat:29.9,
         lng:76.8204121,
         zoom: 15,
-        zoomOffset: 10
+        zoomOffset: 10,
+        markers : [[29.946,76.818],[29.9481, 76.819065],[29.94, 76.8160],[29.94, 76.813],[29.9471, 76.812],[29.946681,76.815]],
+        line1 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        },
+        line2 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        },
+        line3 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        },
+        line4 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        },
+        line5 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        },
+        line6 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        },
+        line7 : {
+            isActive: false,
+            line : helpers.lineString(
+                [
+                  [52.5069704, 13.2846501],
+                  [47.3775499, 8.4666755],
+                ].map(lngLat => [lngLat[1], lngLat[0]])
+              )
+        }
     }
+
+
+   line = helpers.lineString([
+      [52.5069704,13.2846501],
+      [47.3775499,8.4666755],
+      [51.5287718,-0.2416804],
+    ].map(latLng => [latLng[1],latLng[0]]));
+
     getGeoLocation = () => {
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
@@ -39,20 +111,21 @@ class MAP extends Component {
                         height: "1000px",
                         width: "1800px"
                     }}>
-                        <Map style={{
+                        <Map onLocationfound={this.getGeoLocation} style={{
                         paddingBottom: "5%",
                         height: "1000px",
                         width: "1800px"}} center={[this.state.lat, this.state.lng]} zoom={this.state.zoom} zoomOffset={this.zoomOffset}>
                             <TileLayer
                                 attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                                 url="https://api.mapbox.com/styles/v1/rustyraptor/cjkbednp4buod2rnwog2xrdtb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnVzdHlyYXB0b3IiLCJhIjoiY2prOXdtZ2E5MjN3ODNxbWVsM3NyNWlsZCJ9.AVHo6o9Z68w1c2lsBXuGDg"/>
-                            <Marker position={[this.props.coords.longitude, this.props.coords.longitude]}>
+                            <Marker position={[this.state.lat, this.state.lng]}>
                                 <Popup>
                                     A pretty CSS3 popup.
                                     <br/>
                                     Easily customizable.
                                 </Popup>
                             </Marker>
+                            <GeoJSON data={this.line} />
                         </Map>
                     </div>
         : <div>Getting the location data&shellip; </div>);
