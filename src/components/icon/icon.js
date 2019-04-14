cda8d744582bd5a6aac4da96e01c3121278a2c63
war-turtle <kartikyadav71199@gmail.com>
2019-04-14 16:41:22 +0530

    right name added

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,30 +1,30 @@
 const something = {
-  "Kay Sir": {
+  "Karan": {
     iconUrl: "https://user-images.githubusercontent.com/23381512/56082423-56dbc880-5e36-11e9-9a95-e305d1acce94.png",
     iconSize: [40, 40],
     iconAnchor: [0, 0]
   },
-  "Rishabh Sir": {
+  "Rishabh": {
     iconUrl: "https://user-images.githubusercontent.com/23381512/56082493-01ec8200-5e37-11e9-8238-27549b70848e.png",
     iconSize: [40, 40],
     iconAnchor: [0, 0]
   },
-  "Narendra Sir": {
+  "Narendra": {
     iconUrl: "https://user-images.githubusercontent.com/23381512/56082430-6f4be300-5e36-11e9-90b7-e58e51923a26.png",
     iconSize: [40, 40],
     iconAnchor: [0, 0]
   },
-  "Paragi Sir": {
+  "Paragi": {
     iconUrl: "https://user-images.githubusercontent.com/23381512/56082451-91456580-5e36-11e9-93e0-755d5a6185d6.png",
     iconSize: [40, 40],
     iconAnchor: [0, 0]
   },
-  "Pankag Sir": {
+  "Pankaj": {
     iconUrl: "https://user-images.githubusercontent.com/23381512/56082440-7bd03b80-5e36-11e9-8944-5f7d79cd48aa.png",
     iconSize: [40, 40],
     iconAnchor: [0, 0]
   },
-  "Android Sir": {
+  "Arpit": {
     iconUrl: "https://user-images.githubusercontent.com/23381512/56082445-8559a380-5e36-11e9-8a83-3cc5f538e0b9.png",
     iconSize: [40, 40],
     iconAnchor: [0, 0]
@@ -34,14 +34,8 @@ const something = {
     iconSize: [40, 40],
     iconAnchor: [5, 5]
   },
-  "Anuz Sir": {
-    iconUrl:
-      "https://user-images.githubusercontent.com/23381512/56069674-d8354b80-5da1-11e9-867c-1d78e891b9d7.png",
-    iconSize: [40, 40],
-    iconAnchor: [5, 5]
-  },
   "No name": {
-    iconUrl: "https://user-images.githubusercontent.com/23381512/56082445-8559a380-5e36-11e9-8a83-3cc5f538e0b9.png",
+    iconUrl: "https://img.icons8.com/doodle/48/000000/delete-sign.png",
     iconSize: [40, 40],
     iconAnchor: [5, 5]
   }
